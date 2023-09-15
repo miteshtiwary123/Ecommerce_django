b5286ca3392ad5aeb0ca9f8699c5de571445296c
Mitesh Kumar Tiwary <miteshtiwary123@users.noreply.github.com>
2023-09-15 13:18:25 +0530

    Image and description added for product

--- store/models.py
+++ store/models.py
@@ -8,6 +8,9 @@ class Category(models.Model):
     def __str__(self) -> str:
         return self.name
     
+    class Meta:
+        verbose_name_plural = 'categories'
+    
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
