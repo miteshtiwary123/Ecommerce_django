e7a5f3db4cdd91e408f411c76d0ef226d3d5c349
Mitesh Kumar Tiwary <miteshtiwary123@users.noreply.github.com>
2023-10-13 01:02:29 +0530

    registration page added

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 def home(request):
     products = Product.objects.all()
@@ -30,3 +34,23 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You have been logged out..."))
     return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Your account is created successfully..."))
+            return redirect('home')
+        else:
+            messages.success(request, ("There is an error... please try after sometime. "))
+            return redirect('register')
+        
+    else:
+        return render(request, 'register.html', {'form': form})
