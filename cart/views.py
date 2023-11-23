d814e19563c17d5e3c8e713acfb8bfccb030a51f
Mitesh Kumar Tiwary <miteshtiwary123@users.noreply.github.com>
2023-11-23 09:15:25 +0530

    cart quantity updated

--- cart/views.py
+++ cart/views.py
@@ -22,8 +22,11 @@ def cart_add(request):
         # Save to session
         cart.add(product=product)
 
+        # Get cart quantity
+        cart_quantity = cart.__len__()
+
         # Return response
-        response = JsonResponse({'Product Name: ': product.name})
+        response = JsonResponse({'qty': cart_quantity})
         return response
 
 def cart_delete(request):
