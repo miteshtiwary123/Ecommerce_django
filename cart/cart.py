8d15f6c88e0235d0ace830b8e712d8ad6e320560
Mitesh Kumar Tiwary <miteshtiwary123@users.noreply.github.com>
2023-11-23 08:59:07 +0530

    Add to cart view created

--- cart/cart.py
+++ cart/cart.py
@@ -12,3 +12,13 @@ class Cart():
 
         # make sure cart is available on all page of site
         self.cart = cart 
+
+    def add(self, product):
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+
+        self.session.modified = True
